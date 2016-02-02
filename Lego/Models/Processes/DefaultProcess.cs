9b5b0bd0df2e54d068b4593dcb1b576c872ac4d7
Aldi Alimucaj <aldi.alimucaj@gmail.com>
2016-02-02 22:12:44 +0100

    [FEATURE] specific process definition

---
+++ Lego/Models/Processes/DefaultProcess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lego.Models.Processes
+{
+    class DefaultProcess : IProcess
+    {
+        ProcessStartInfo Info { get; set; }
+
+        public DefaultProcess(LgProcess process)
+        {
+            Info = new ProcessStartInfo(process.FullPath);
+        }
+
+        public void AddArgument(string arg)
+        {
+            Info.Arguments += " " + arg;
+        }
+
+        public ProcessStartInfo GetStartInfo()
+        {
+            return Info;
+        }
+    }
+}
